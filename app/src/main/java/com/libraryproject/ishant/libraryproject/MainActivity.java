994a665fb229b6fb14a77f0d5d2ee4779737e55d
ishantrana <ishant@atcouch.com>
2016-07-03 15:32:49 +0530

    called toast

--- app/src/main/java/com/libraryproject/ishant/libraryproject/MainActivity.java
+++ app/src/main/java/com/libraryproject/ishant/libraryproject/MainActivity.java
@@ -3,11 +3,14 @@ package com.libraryproject.ishant.libraryproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ishant.mylibrary.MyLibrary;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyLibrary.getOurinstance().showToast(this,"Ishant");
     }
 }
